40c90476c9048f4e667a102b3c7acb8fcecc8350
DavidXiaoJunYu <2203574@sit.singaporetech.edu.sg>
2023-03-22 18:15:46 +0800

    Splash Screen completed

--- Ready_Set_Jump/Ready_Set_Jump/SplashScreen.cpp
+++ Ready_Set_Jump/Ready_Set_Jump/SplashScreen.cpp
@@ -1,13 +1,16 @@
 #include "AEEngine.h"
 #include "GameStateManager.hpp"
 #include "GameObjClasses.hpp"
+#include "Ultilities.hpp"
 
 GameObject SplashScreen;
 AEGfxTexture* SplashScreenTexture{ nullptr };
+float maxSplashScreenTime = 4.f;
+bool fadingIn = true;
 void SplashScreen_Load() {
 	MakeMesh();
 	SplashScreenTexture = AEGfxTextureLoad("Assets/Images/DigiPen_BLACK.png");
-	SplashScreen = GameObject({ 0.0f, 0.0f }, { 1000.f,500.f }, {1.0f, 1.0f, 1.0f, 1.0f}, 0.f, AE_GFX_RM_TEXTURE);
+	SplashScreen = GameObject({ 0.0f, 0.0f }, { 1120.f,400.f }, {1.0f, 1.0f, 1.0f, 0.0f}, 0.f, AE_GFX_RM_TEXTURE);
 	SplashScreen.SetTexture(SplashScreenTexture);
 
 }
@@ -17,9 +20,26 @@ void SplashScreen_Initialize() {
 }
 
 void SplashScreen_Update() {
-	if (AEInputCheckTriggered(AEVK_LBUTTON)) {
+	if (AEInputCheckTriggered(AEVK_LBUTTON) || AEInputCheckTriggered(AEVK_SPACE) || maxSplashScreenTime <=0.f) {
 		next = GS_MAINMENU;
 	}
+	if (maxSplashScreenTime > 0.f) {
+		maxSplashScreenTime -= e_deltaTime;
+		if (fadingIn) {
+			ColourValue temp = SplashScreen.GetColour();
+			temp.alpha += e_deltaTime;
+			SplashScreen.SetColour(temp);
+			if (SplashScreen.GetColour().alpha >= 1.f) fadingIn = false;
+		}
+		else
+		{
+			ColourValue temp = SplashScreen.GetColour();
+			temp.alpha -= e_deltaTime;
+			SplashScreen.SetColour(temp);
+			if (SplashScreen.GetColour().alpha <= 0.f) fadingIn = true;
+		}
+	}
+
 }
 
 void SplashScreen_Draw() {
@@ -32,6 +52,6 @@ void SplashScreen_Free() {
 }
 
 void SplashScreen_Unload() {
-	delete SplashScreenTexture;
+	AEGfxTextureUnload(SplashScreenTexture);
 	AEGfxMeshFree(pMesh);
 }
