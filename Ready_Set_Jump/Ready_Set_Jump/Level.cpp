5d26656fdea2dc19f2115a80fe524a1bfee4c8b9
AdrianPeh <2200722@sit.singaporetech.edu.sg>
2023-03-29 18:52:16 +0800

    infinite lifetime

--- Ready_Set_Jump/Ready_Set_Jump/Level.cpp
+++ Ready_Set_Jump/Ready_Set_Jump/Level.cpp
@@ -270,17 +270,18 @@ void Level_Update()
 	*(particleList + (frameCounter % MAX_PARTICLE_NUMBER)) = GameObject({ particalPosition.x, particalPosition.y }, { particalScale.x, particalScale.y }, { 255.f, 255.f, 255.f, 255.f },
 					static_cast<float>(randLifeTime), AE_GFX_RM_COLOR, pMesh, { particleVelocity.x , particleVelocity.y });*/
 
-	if (frameCounter % 2) {
+	if (frameCounter % 3) {
 		AEVec2 particalPosition = { static_cast<float>(Player.GetPosition().x + WINDOW_WIDTH), static_cast<float>(rand() % static_cast<int>(e_binaryMapHeight * GRID_HEIGHT_SIZE) - static_cast<float>(e_binaryMapHeight * GRID_HEIGHT_SIZE) + HALVE_WINDOW_HEIGHT) };
 
 
 		int randScale = rand() % 8 + 2;
 		AEVec2 particalScale = { static_cast<float>(randScale), static_cast<float>(randScale) };
 
-		int randLifeTime = rand() % 70 + 1.5 * e_binaryMapWidth;
+		//int randLifeTime = rand() % 70 + 1.5 * e_binaryMapWidth;
+		int randLifeTime = rand() % 70 + 10000;
 		AEVec2 particleVelocity = { -static_cast<float>(rand() % 5 + 2) , 0.0f };
 
-		*(particleList + (static_cast<int>(frameCounter*0.5) % MAX_PARTICLE_NUMBER)) = GameObject({ particalPosition.x, particalPosition.y }, { particalScale.x, particalScale.y }, { 255.f, 255.f, 255.f, 255.f },
+		*(particleList + (static_cast<int>(frameCounter*0.33) % MAX_PARTICLE_NUMBER)) = GameObject({ particalPosition.x, particalPosition.y }, { particalScale.x, particalScale.y }, { 255.f, 255.f, 255.f, 255.f },
 			static_cast<float>(randLifeTime), AE_GFX_RM_COLOR, pMesh, { particleVelocity.x , particleVelocity.y });
 	}
 
