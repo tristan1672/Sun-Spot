73fbac8f11c806964ac96f67f402782e3f2acd04
AdrianPeh <112685376+AdrianPeh@users.noreply.github.com>
2023-04-01 21:47:46 +0800

    Updated text

--- Ready_Set_Jump/Ready_Set_Jump/PauseMenu.cpp
+++ Ready_Set_Jump/Ready_Set_Jump/PauseMenu.cpp
@@ -13,17 +13,21 @@
 #include "UI.hpp"
 #include "GameStateManager.hpp"
 namespace PauseMenu {
-	UIText* pauseText;
-	UIText* resumeText;
-	UIText* mainMenuText;
-	UIText* quitText;
+	UIText* buttonText[4];
+
 	GameObject* backGround;
 	void CreatePauseMenu() {
 		// allocate memory for each button and text
-		pauseText = new UIText{ UIText("PAUSED", { -0.15f, 0.6f }, { 1.f,1.f }, White) };
-		resumeText = new UIText{ UIText("Resume", { -0.15f, 0.3f }, { 1.f,1.f }, White, true, GreenTea) };
-		mainMenuText = new UIText{ UIText("Main Menu", { -0.2f, 0.f }, { 1.f,1.f }, White, true, GreenTea) };
-		quitText = new UIText{ UIText("Quit", { -0.09f, -0.3f }, { 1.f,1.f }, White, true, GreenTea) };
+		buttonText[0] = new UIText{ UIText("PAUSED", { 0.f, 0.5f }, { 1.f,1.f }, White) };
+		buttonText[1] = new UIText{ UIText("Resume", { 0.f, 0.25f }, { 1.f,1.f }, White, true, GreenTea) };
+		buttonText[2] = new UIText{ UIText("Main Menu", { 0.f, 0.05f }, { 1.f,1.f }, White, true, GreenTea) };
+		buttonText[3] = new UIText{ UIText("Quit", { 0.f, -0.15f }, { 1.f,1.f }, White, true, GreenTea) };
+
+		for (unsigned int i{}; i < 4; ++i) {
+			AEVec2 size{};
+			AEGfxGetPrintSize(e_fontID, buttonText[i]->GetText(), buttonText[i]->GetScale().x, size.x, size.y);
+			buttonText[i]->SetPosition({ -0.5f * size.x + buttonText[i]->GetPosition().x, buttonText[i]->GetPosition().y });
+		}
 
 		backGround = new GameObject{ GameObject({-0.1f,0.f},{2000,1000},Black) };
 	}
@@ -31,37 +35,33 @@ namespace PauseMenu {
 		//sets behaviour what each button does
 		AEGfxSetCamPosition(0.f, 0.f);
 		if (AEInputCheckTriggered(AEVK_LBUTTON)) {
-			if (resumeText->MouseCollision(mouse)) level_state = PLAYING;// return to gaming
-			if (mainMenuText->MouseCollision(mouse)) e_next_state = GS_MAINMENU;// return to main menu
-			if (quitText->MouseCollision(mouse)) e_next_state = GS_QUIT;// quits the game
+			if (buttonText[1]->MouseCollision(mouse)) level_state = PLAYING;// return to gaming
+			if (buttonText[2]->MouseCollision(mouse)) e_next_state = GS_MAINMENU;// return to main menu
+			if (buttonText[3]->MouseCollision(mouse)) e_next_state = GS_QUIT;// quits the game
 		}
 
 		/* text box animation to fade in/out on hover*/
-		if (resumeText->MouseCollision(mouse) && resumeText->GetTextBoxAlpha() < 1.f) {
-			resumeText->TextBoxFadeIn();
-		}else if (!(resumeText->MouseCollision(mouse))) resumeText->TextBoxFadeOut();
+		if (buttonText[1]->MouseCollision(mouse) && buttonText[1]->GetTextBoxAlpha() < 1.f) {
+			buttonText[1]->TextBoxFadeIn();
+		}else if (!(buttonText[1]->MouseCollision(mouse))) buttonText[1]->TextBoxFadeOut();
 
-		if (mainMenuText->MouseCollision(mouse) && mainMenuText->GetTextBoxAlpha() < 1.f) {
-			mainMenuText->TextBoxFadeIn();
-		}else if (!(mainMenuText->MouseCollision(mouse))) mainMenuText->TextBoxFadeOut();
+		if (buttonText[2]->MouseCollision(mouse) && buttonText[2]->GetTextBoxAlpha() < 1.f) {
+			buttonText[2]->TextBoxFadeIn();
+		}else if (!(buttonText[2]->MouseCollision(mouse))) buttonText[2]->TextBoxFadeOut();
 
-		if (quitText->MouseCollision(mouse) && quitText->GetTextBoxAlpha() < 1.f) {
-			quitText->TextBoxFadeIn();
-		}else if (!(quitText->MouseCollision(mouse))) quitText->TextBoxFadeOut();
+		if (buttonText[3]->MouseCollision(mouse) && buttonText[3]->GetTextBoxAlpha() < 1.f) {
+			buttonText[3]->TextBoxFadeIn();
+		}else if (!(buttonText[3]->MouseCollision(mouse))) buttonText[3]->TextBoxFadeOut();
 
 	}
 	void DrawPauseMenu() {//draws the buttons and text
 		backGround->DrawObj();
-		pauseText->DrawObj();
-		quitText->DrawObj();
-		resumeText->DrawObj();
-		mainMenuText->DrawObj();
+
+		for (size_t i{}; i < sizeof buttonText / sizeof buttonText[0]; ++i)
+			buttonText[i]->DrawObj();
 	}
 	void FreePauseMenu() {
-		delete pauseText;
-		delete quitText;
-		delete resumeText;
-		delete mainMenuText;
-		delete backGround;
+		for (size_t i{}; i < sizeof buttonText / sizeof buttonText[0]; ++i)
+			delete buttonText[i];
 	}
 }
